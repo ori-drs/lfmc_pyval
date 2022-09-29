7e2c14c4599b735f9157246ff054ebd5ea37d770
Siddhant Gangapurwala <34548119+gsiddhant@users.noreply.github.com>
2022-09-29 15:09:48 +0100

    update project url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='lfmc_pyval',
     version='0.1',
     packages=find_packages(),
-    url='https://bitbucket.org/gsiddhant/lfmc_pyval',
+    url='https://github.com/ori-drs/lfmc_pyval',
     author='Siddhant Gangapurwala',
     author_email='siddhant@robots.ox.ac.uk',
     python_requires='>=3.6.0,<3.9.0',
